b21ddc8524e143386d2d7f3c2218c1d365b07059
Oleg <oleg.harlamov@ivkhk.ee>
2020-09-16 12:23:43 +0300

    le final

--- src/sptvr19myarray/App.java
+++ src/sptvr19myarray/App.java
@@ -15,6 +15,7 @@ import java.util.Random;
 class App {
     private final Random rand = new Random();
     public void run(){
+        System.out.printf("---MyArray---%nЗадание:%nСоздайте массив из 20 случайных целых чисел%nВычислите среднее арифметическое элементов массива%nбез учёта минимального и максимального элементов массива.%nРешение:%nМассив целых чётных чисел:%n");
         double sum = 0;
         int[]nums = new int[20];
         for (int i = 0;i< nums.length;i++){
@@ -23,15 +24,17 @@ class App {
             }while (nums[i] % 2 !=0);
         }
         for (int i = 0; i< nums.length ; i++){
-            System.out.println(nums[i]);
+            System.out.printf("%12d",nums[i]);
         }
         Arrays.sort(nums);
+        int min = nums[0];
+        int max = nums[nums.length-1];
         nums[0] = 0;
         nums[19] = 0;
         for (int i = 0; i< nums.length ; i++){
             sum +=nums[i];
         }
-        System.out.printf("%f", sum/18);
+        System.out.printf("%nСреднее арифметическое без учёта максимального (%d) и минимального(%d): %.2f%n",max,min,sum/18);
     }
     
 }
